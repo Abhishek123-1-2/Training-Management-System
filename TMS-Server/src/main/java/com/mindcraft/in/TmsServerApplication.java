03a10bd731057ca3098244b6ce36b7030b8183dd
Abhishek Pillai <121553063+Abhishek-pillai-123@users.noreply.github.com>
2023-11-12 09:11:44 +0530

    Initital Commit
    Initial Commit

---
+++ TMS-Server/src/main/java/com/mindcraft/in/TmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.mindcraft.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsServerApplication.class, args);
+	}
+
+}
